971cc631f067dce223b3899eef90beb327fb72da
Josh Barton <jbarton2010@gmail.com>
2018-07-18 16:51:45 -0700

    Initial Commit

---
+++ HangmanGame/Rules.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace HangmanGame
+{
+    public static class Rules
+    {
+        public static bool isALetter(char input)
+        {
+            return Regex.IsMatch(input.ToString(), "[a-z]", RegexOptions.IgnoreCase);
+        }
+
+        public static bool letterContains(string word, char input)
+        {
+            return Regex.IsMatch(word, input.ToString(), RegexOptions.IgnoreCase);
+        }
+    }
+}
---
+++ HangmanGameTests/RulesTests.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HangmanGame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangmanGame.Tests
+{
+    [TestClass()]
+    public class RulesTests
+    {
+        [TestMethod()]
+        public void isALetter_TestForLowercase()
+        {
+            char input = 'a';
+
+            Assert.IsTrue(Rules.isALetter(input));
+        }
+
+        [TestMethod()]
+        public void isALetter_TestForUppercase()
+        {
+            char input = 'A';
+
+            Assert.IsTrue(Rules.isALetter(input));
+        }
+
+        [TestMethod()]
+        public void isALetter_TestForNumber()
+        {
+            char input = '5';
+
+            Assert.IsFalse(Rules.isALetter(input));
+        }
+
+        [TestMethod()]
+        public void isALetter_TestForSpecialCharacter()
+        {
+            char input = '@';
+
+            Assert.IsFalse(Rules.isALetter(input));
+        }
+
+        [TestMethod()]
+        public void letterContains_TestForMatchingLowercaseLetter()
+        {
+            string word = "Animal";
+            char input = 'a';
+
+            Assert.IsTrue(Rules.letterContains(word, input));
+        }
+
+        [TestMethod()]
+        public void letterContains_TestForMatchingUpperLetter()
+        {
+            string word = "Animal";
+            char input = 'M';
+
+            Assert.IsTrue(Rules.letterContains(word, input));
+        }
+
+        [TestMethod()]
+        public void letterContains_TestForNonMatchingInput()
+        {
+            string word = "Animal";
+            char input = '4';
+
+            Assert.IsFalse(Rules.letterContains(word, input));
+        }
+    }
+}
