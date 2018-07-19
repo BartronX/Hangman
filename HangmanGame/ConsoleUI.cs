9ab4052b31b01e7cdef959a32dbd3034205c3f82
Josh Barton <jbarton2010@gmail.com>
2018-07-19 16:00:41 -0700

    Started Console UI

---
+++ HangmanGame/ConsoleUI.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangmanGame
+{
+    public static class ConsoleUI
+    {
+        public static void PrintMainMenuHeader()
+        {
+            Console.WriteLine("*** Hangman ***");
+        }
+
+        public static void PrintMainMenuOptions()
+        {
+            Console.WriteLine("(1) Play");
+            Console.WriteLine("(2) View Rules");
+            Console.WriteLine("(3) Word List");
+            Console.WriteLine("(4) Quit");
+        }
+    }
+}
